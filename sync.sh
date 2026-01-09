69c68b18af0cecbdd2b07fe7a3d28a92f25dfa58
foxxie_911 <foxxie911.dev@proton.me>
2026-01-09 12:40:41 +0600

    rofi added for better dmenu

--- sync.sh
+++ sync.sh
@@ -6,32 +6,33 @@ declare -A folders
 folders[i3]="$HOME/.config/i3"
 folders[i3status]="$HOME/.config/i3status"
 folders[picom]="$HOME/.config/picom"
+folders[rofi]="$HOME/.config/rofi"
 folders[kitty]="$HOME/.config/kitty"
 folders[CustomBinary]="$HOME/.local/bin"
 folders[wallpapers]="$HOME/.wallpapers"
 
 for folder in "${folders[@]}"; do
-  cp -r "$folder" ./
-  echo "Copied: ${folder}"
+    cp -r "$folder" ./
+    echo "Copied: ${folder}"
 done
 
 # Git add and commit
 function gitcommit() {
-  git add .
-  git commit -m "$1"
-  git push -u origin
+    git add .
+    git commit -m "$1"
+    git push -u origin
 }
 
 while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do
-  case "$1" in
-  -m | --message)
+    case "$1" in
+    -m | --message)
+        shift
+        gitcommit "$1"
+        ;;
+    -v | --version)
+        echo "1.0"
+        ;;
+    esac
     shift
-    gitcommit "$1"
-    ;;
-  -v | --version)
-    echo "1.0"
-    ;;
-  esac
-  shift
 done
 if [[ "$1" == '--' ]]; then shift; fi
